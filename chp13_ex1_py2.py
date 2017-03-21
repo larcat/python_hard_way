a16fa6dd5db6e8926587436122d67c999fdd8962
larcat <larcatdm@gmail.com>
2017-03-21 18:01:10 -0400

    Next 3 exercises.

---
+++ chp13_ex1_py2.py
@@ -0,0 +1,8 @@
+from sys import argv
+
+script, first, second = argv
+
+print "The script is called:", script
+print "Your first variable is:", int(first)
+print "Your second variable is:", int(second)
+print "Those two added is:", int(first) + int(second)
