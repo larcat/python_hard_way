ca335034eaf647a7c7f0afa9b24c73693d731c4e
larcat <larcatdm@gmail.com>
2017-05-30 10:25:42 -0500

    Chap 22

---
+++ chp22_py2.py
@@ -0,0 +1,29 @@
+# Print -- Makes text appear on screen.
+# # Comments out lines
+# + - / * % Arithmetic. % is modulus
+# , separates arguments
+# = Assigns values
+# % after print and in print prints the corresponding variable
+# round() rounds -- needs an argument round(val, num digits)
+# True False boolean assignation
+# %r, %s, %d -- %r and %s are string formatters, %d is numerical
+    # Actual diff?
+# print """ prints a block of text needs """ to end the block as well.
+# \n in a print statement is a new line, good ole regex
+# \ before a regex special char escapes out the char like \\
+# \t inserts a tab
+# raw_input() takes user input and then uses it in the rest of the script
+    # Text as an arg prompts the user with that string.
+# from sys import imports a function for use in your script
+# int() does type conversion to integer
+# argv allows you to give arguments to command line script calls for us in the script
+# open() opens a connection to a file for readin, etc
+# close() closes a connection to a file
+# truncate() erases text from an open connection
+# write() writes lines to an open connection
+# read() reads an open file in so you can use the data in the script
+# def funcname defines a function for later use in the script
+# seek(num) goes to num (an int) in an open file. Indexing starts at 0.
+
+
+
