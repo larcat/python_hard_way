73c18f7e86710603bc215219503e01c28997ffa7
larcat <larcatdm@gmail.com>
2017-05-30 12:22:59 -0500

    23

---
+++ chp23_py2.py
@@ -0,0 +1,8 @@
+# some rando's utility functions @ https://gist.github.com/shazow/603374
+
+# xrange() -- Think this is a shortened version of range that defaults to begin @ 0?
+# KeyError() some sort of error that is raised -- specific kind?
+#defaultdict() -- Think this allocates an empty dictionary?
+# yield -- Think this brings a value out of a loop somehow in a clever way?
+# * and ** in args to function -- Some sorta dark magic here.
+# zip() no clue on this guy
